bfe5659711a62696b6a7508914d6469bbe641d34
JASS63 <124114911+JASS63@users.noreply.github.com>
2024-03-07 23:54:35 +0530

    Update and rename exp.37 FCFS disk scheduling.cpp to Q.37 FCFS disk scheduling.cpp

--- Q.37 FCFS disk scheduling.cpp
+++ Q.37 FCFS disk scheduling.cpp
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
-int main() {
+int main() 
+
+{
     int n, head, seek_time = 0;
     printf("Enter the number of disk requests: ");
     scanf("%d", &n);
@@ -8,6 +10,7 @@ int main() {
     printf("Enter the disk request queue:\n");
     for (int i = 0; i < n; i++) {
         scanf("%d", &request_queue[i]);
+        
     }
     printf("Enter the initial position of the disk head: ");
     scanf("%d", &head);
@@ -18,6 +21,7 @@ int main() {
         head = request_queue[i];
         printf(" -> %d", head);
     }
+    
     printf("\nTotal Seek Time: %d\n", seek_time);
     printf("Average Seek Time: %.2f\n", (float) seek_time / n);
     return 0;
