70a30c047fbe3261ba8698b90ada6804f7718e77
JASS63 <124114911+JASS63@users.noreply.github.com>
2024-03-07 23:38:22 +0530

    Update and rename exp.28 simulating  func GREP UNIX Command.cpp to Q.28 simulating  func GREP UNIX Command.cpp

--- Q.28 simulating  func GREP UNIX Command.cpp
+++ Q.28 simulating  func GREP UNIX Command.cpp
@@ -1,17 +1,22 @@
 #include <stdio.h>
+#include<conio.h>
 #include <stdlib.h>
 #include <string.h>
 #define MAX_LINE_LENGTH 1024
-void grepFile(const char *filename, const char *pattern) {
+void grepFile(const char *filename, const char *pattern) 
+
     FILE *file = fopen(filename, "r");
-    if (file == NULL) {
+    if (file == NULL) 
+    {
         perror("Error opening file");
         exit(EXIT_FAILURE);
     }
     char line[MAX_LINE_LENGTH];
     int line_number = 0;
     printf("Lines matching '%s' in file '%s':\n", pattern, filename);
-    while (fgets(line, sizeof(line), file) != NULL) {
+    while (fgets(line, sizeof(line), file) != NULL) 
+    
+    {
         line_number++;
         if (strstr(line, pattern) != NULL) {
             printf("%d: %s", line_number, line);
